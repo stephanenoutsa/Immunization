3a9afc70720f38967673d7b0ae69a526f1e72935
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-04-24 19:52:35 +0100

    Adjusts the messaging schedule and improves the messaging logic

--- app/src/main/java/motherslove/org/immunization/MyService.java
+++ app/src/main/java/motherslove/org/immunization/MyService.java
@@ -58,6 +58,7 @@ public class MyService extends Service {
                         Toast.makeText(cont, "Checking contacts...", Toast.LENGTH_SHORT).show();
                         String url = "http://api.wasamundi.com/v2/texto/send_sms?user=motherslove&api_key=nKjahG4T&from=MothersLove&to=237";
                         //String url = "http://api.wasamundi.com/v2/texto/send_sms?user=humphrey&api_key=7vLIs7YY&from=MothersLove&to=237";
+                        String smsBody = "";
                         final String phone = cn.getContactphone();
                         //Toast.makeText(cont, "Phone number is " + phone, Toast.LENGTH_SHORT).show();
                         String dob = cn.getContactdob();
@@ -69,19 +70,20 @@ public class MyService extends Service {
                         //Toast.makeText(cont, "Diff is " + diff, Toast.LENGTH_SHORT).show();
                         received = cn.getContactreceived();
                         String lang = cn.getContactlanguage();
+                        //Toast.makeText(cont, lang, Toast.LENGTH_SHORT).show();
 
                         // Welcome message
                         if(diff < 366) {
                             if (received == 0) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.welcome_msg_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.welcome_msg_en), "UTF-8");
+                                    //Toast.makeText(cont, lang + " is English", Toast.LENGTH_SHORT).show();
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.welcome_msg_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.welcome_msg_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.welcome_msg_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -95,28 +97,28 @@ public class MyService extends Service {
                                 messageApiCaller.execute(url1);
 
                                 // Increment received messages count
-                                if (diff < 21) {
+                                if (diff < 12) {
                                     received = 1;
                                 }
-                                else if (diff >= 21 && diff < 40) {
+                                else if (diff >= 12 && diff < 26) {
                                     received = 2;
                                 }
-                                else if (diff >= 40 && diff < 70) {
+                                else if (diff >= 26 && diff < 39) {
                                     received = 3;
                                 }
-                                else if (diff >= 70 && diff < 96) {
+                                else if (diff >= 39 && diff < 49) {
                                     received = 4;
                                 }
-                                else if (diff >= 96 && diff < 270) {
+                                else if (diff >= 49 && diff < 77) {
                                     received = 5;
                                 }
-                                else if (diff >= 270 && diff < 365) {
+                                else if (diff >= 77 && diff < 186) {
                                     received = 6;
                                 }
-                                else if (diff == 365) {
+                                else if (diff >= 186 && diff < 273) {
                                     received = 7;
                                 }
-                                else if (diff == 366) {
+                                else if (diff >= 273 && diff < 302) {
                                     received = 8;
                                 }
 
@@ -132,17 +134,16 @@ public class MyService extends Service {
                         }
 
                         // Week one message
-                        if(diff >= 7 && diff < 21) {
+                        if(diff >= 4 && diff < 12) {
                             if(received == 1) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg2_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg2_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg2_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg2_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg2_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -170,17 +171,16 @@ public class MyService extends Service {
                         }
 
                         // Week three message
-                        else if(diff >= 21 && diff < 40) {
+                        else if(diff >= 12 && diff < 26) {
                             if(received == 2) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg3_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg3_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg3_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg3_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg3_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -208,17 +208,16 @@ public class MyService extends Service {
                         }
 
                         // Week six message
-                        else if(diff >= 40 && diff < 70) {
+                        else if(diff >= 26 && diff < 39) {
                             if(received == 3) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg4_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg4_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg4_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg4_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg4_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -246,17 +245,16 @@ public class MyService extends Service {
                         }
 
                         // Week ten message
-                        else if(diff >= 70 && diff < 96) {
+                        else if(diff >= 39 && diff < 49) {
                             if(received == 4) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg5_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg5_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg5_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg5_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg5_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -284,17 +282,16 @@ public class MyService extends Service {
                         }
 
                         // Week fourteen message
-                        else if(diff >= 96 && diff < 270) {
+                        else if(diff >= 67 && diff < 77) {
                             if(received == 5) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg6_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg6_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg6_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg6_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg6_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -321,18 +318,17 @@ public class MyService extends Service {
                             }
                         }
 
-                        // Month nine message
-                        else if(diff >= 270 && diff < 365) {
+                        // Week 14 message (message 7)
+                        else if(diff >= 95 && diff < 105) {
                             if(received == 6) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg7_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg7_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg7_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg7_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg7_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -359,18 +355,17 @@ public class MyService extends Service {
                             }
                         }
 
-                        // Year one message
-                        else if(diff == 365) {
+                        // Month 6 message
+                        else if(diff >= 168 && diff < 197) {
                             if(received == 7) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg8_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg8_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg8_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg8_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg8_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
@@ -398,17 +393,16 @@ public class MyService extends Service {
                         }
 
                         // Conclusion message
-                        else if(diff == 366) {
+                        else if(diff >= 273 && diff < 302) {
                             if(received == 8) {
-                                String smsBody;
-                                if (lang == "English") {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg9_en), "UTF-8");
+                                if (lang.equals("English")) {
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg9_en), "UTF-8");
                                 }
                                 else {
-                                    smsBody = URLEncoder.encode(getResources().getString(R.string.msg9_fr), "UTF-8");
+                                    smsBody += URLEncoder.encode(getResources().getString(R.string.msg9_fr), "UTF-8");
                                 }
 
-                                Toast.makeText(cont, getResources().getString(R.string.msg9_en), Toast.LENGTH_LONG).show();
+                                //Toast.makeText(cont, smsBody, Toast.LENGTH_LONG).show();
 
                                 // Send a text-based SMS
                                 Toast.makeText(cont, "Preparing to call message api", Toast.LENGTH_SHORT).show();
